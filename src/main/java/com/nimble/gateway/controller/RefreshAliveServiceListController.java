4de8e1ead8a0f3e1817754ab0bb7b9a740851b08
yanqiuhuang188 <yanqiuhuang188@163.com>
2018-06-12 01:09:51 +0800

    update

---
+++ src/main/java/com/nimble/gateway/controller/RefreshAliveServiceListController.java
@@ -0,0 +1,11 @@
+package com.nimble.gateway.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RefreshAliveServiceListController {
+
+    public void test(){
+    }
+
+}
