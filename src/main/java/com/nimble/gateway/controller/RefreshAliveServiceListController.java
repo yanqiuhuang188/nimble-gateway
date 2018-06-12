7f3bb7f468b1601008a7c2e0ad3adacb23690569
yanqiuhuang188 <yanqiuhuang188@163.com>
2018-06-13 00:32:03 +0800

    update

--- src/main/java/com/nimble/gateway/controller/RefreshAliveServiceListController.java
+++ src/main/java/com/nimble/gateway/controller/RefreshAliveServiceListController.java
@@ -1,11 +1,20 @@
 package com.nimble.gateway.controller;
 
+import com.netflix.discovery.DiscoveryManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RefreshAliveServiceListController {
 
-    public void test(){
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @GetMapping("/offline")
+    public void offline(){
+        DiscoveryManager.getInstance().shutdownComponent();
     }
 
 }
